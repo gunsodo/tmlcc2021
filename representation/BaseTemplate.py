fc2d3f5cf2f090a775e58235003b01e47c6a75cb
Guntitat Sawadwuthikul <guntitat_sawadwuthikul@hotmail.com>
2021-10-02 23:36:17 +0700

    Reorgainze representation folder

--- representation/BaseTemplate.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-
-class BaseTemplate(ABC):
-    def __init__(self):
-        pass
-    
-    @abstractmethod
-    def represent(self, data):
-        pass
--- representation/WordEmbedding.py
+++
@@ -1,8 +0,0 @@
-from representation.BaseTemplate import BaseTemplate
-
-class WordEmbedding(BaseTemplate):
-    def __init__(self):
-        pass
-
-    def represent(self, data):
-        raise NotImplementedError
