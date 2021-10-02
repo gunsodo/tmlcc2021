b67b364bbdcc7f87c041a0082a2de543f8ccddbe
Guntitat Sawadwuthikul <guntitat_sawadwuthikul@hotmail.com>
2021-10-02 23:33:00 +0700

    Add coordinates data

--- preprocess/coordinates.py
+++ preprocess/coordinates.py
@@ -4,14 +4,14 @@ import os
 import pandas as pd
 from tqdm import tqdm
 
-base_path = "/Users/gunsodo/Desktop/GunSoDo/Workspace/tmlcc/tmlcc-2021/mof_cif_train"
+base_path = "your/path/mof_cif_pretest"
 
 atoms = []
 coords = []
 filenames = []
 errors = []
 
-files = sorted(os.listdir(base_path))[60000:]
+files = sorted(os.listdir(base_path))
 
 for file in tqdm(files):
     full_path = os.path.join(base_path, file)
@@ -24,14 +24,6 @@ for file in tqdm(files):
         errors.append(os.path.splitext(file)[0])
 
 df = pd.DataFrame({'MOFName': filenames, 'atoms': atoms, 'coords': coords})
-df.to_csv("../results/train_coordinates_7.csv", index=False)
+df.to_csv("../results/output.csv", index=False)
 print(errors)
-
-# print(type(atom_full), type(coord_full))
-
-# my_plot = oc.draw.DrawComplex_Matplotlib(atom=atom_full, coord=coord_full)
-# my_plot.add_atom()
-# my_plot.add_bond()
-# my_plot.fig.suptitle("")
-# my_plot.save_img(f"../results/pics/{file[:-4]}")
 
